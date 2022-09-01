dbf5c7f5a29bc2c45f996cf0267c88cd84a99605
Damian Jankowski <damian@datagentleman.io>
2022-09-01 22:12:29 +0200

    RLP tests are passing

--- pkg/rlp/rlp.go
+++ pkg/rlp/rlp.go
@@ -13,21 +13,37 @@ func Encode(values ...interface{}) []byte {
 	for _, v := range values {
 		if isList(v) && !isByteArray(v) {
 			encodedList := Encode(toList(v)...)
+			firstByte := 0
 
-			firstByte := 0xc0 + len(bytes.TrimLeft(encodedList, "\x00"))
+			if len(bytes.TrimLeft(encodedList, "\x00")) <= 55 {
+				firstByte = 0xc0 + len(bytes.TrimLeft(encodedList, "\x00"))
 
-			encoding = append(encoding, byte(firstByte))
-			encoding = append(encoding, bytes.TrimLeft(encodedList, "\x00")...)
+				encoding = append(encoding, byte(firstByte))
+				encoding = append(encoding, bytes.TrimLeft(encodedList, "\x00")...)
+			} else {
+				lenInBytes, _ := toBytes(len(encodedList))
+				lenInBytes = bytes.TrimLeft(lenInBytes, "\x00")
+
+				firstByte = 0xf7 + len(lenInBytes)
+
+				encoding = append(encoding, byte(firstByte))
+				encoding = append(encoding, lenInBytes...)
+				encoding = append(encoding, bytes.TrimLeft(encodedList, "\x00")...)
+			}
 		} else {
 			bs, _ := toBytes(v)
 
-			if len(bytes.TrimLeft(bs, "\x00")) == 1 && bytes.TrimLeft(bs, "\x00")[0] <= 0x7f {
-				encoding = append(encoding, bytes.TrimLeft(bs, "\x00")[0])
+			if !isByteArray(v) {
+				bs = bytes.TrimLeft(bs, "\x00")
+			}
+
+			if len(bs) == 1 && bs[0] <= 0x7f {
+				encoding = append(encoding, bs[0])
 			} else if len(bs) <= 55 {
-				firstByte := 0x80 + len(bytes.TrimLeft(bs, "\x00"))
+				firstByte := 0x80 + len(bs)
 
 				encoding = append(encoding, byte(firstByte))
-				encoding = append(encoding, bytes.TrimLeft(bs, "\x00")...)
+				encoding = append(encoding, bs...)
 			} else {
 				lenInBytes, _ := toBytes(len(bs))
 				lenInBytes = bytes.TrimLeft(lenInBytes, "\x00")
@@ -48,16 +64,30 @@ func isList(v interface{}) bool {
 	slice := reflect.TypeOf(v).Kind() == reflect.Slice
 	array := reflect.TypeOf(v).Kind() == reflect.Array
 
-	return (slice || array)
+	pttr := reflect.TypeOf(v)
+	if pttr.Kind() == reflect.Pointer {
+		aa := pttr.Elem().Kind() == reflect.Slice || pttr.Elem().Kind() == reflect.Array
+		return aa
+	}
+
+	return slice || array
+}
+
+func isPointer(v interface{}) bool {
+	pttr := reflect.TypeOf(v)
+	return pttr.Kind() == reflect.Pointer
 }
 
 func isByteArray(v interface{}) bool {
 	if isList(v) {
-		switch v.(type) {
-		case []uint8:
+		typ := reflect.TypeOf(v)
+
+		if isPointer(v) && typ.Elem().Elem().Kind() == reflect.Uint8 {
 			return true
-		default:
-			return false
+		}
+
+		if typ.Elem().Kind() == reflect.Uint8 {
+			return true
 		}
 	}
 
@@ -72,6 +102,11 @@ func toList(v interface{}) []interface{} {
 	s := []interface{}{}
 	l := reflect.ValueOf(v)
 
+	ll := reflect.ValueOf(v)
+	if ll.IsNil() {
+		return s
+	}
+
 	for i := 0; i < l.Len(); i++ {
 		s = append(s, l.Index(i).Interface())
 	}
@@ -80,15 +115,24 @@ func toList(v interface{}) []interface{} {
 }
 
 func toBytes(v interface{}) ([]byte, error) {
+	if isPointer(v) && reflect.ValueOf(v).IsNil() {
+		return []byte{}, nil
+	}
+
 	switch t := v.(type) {
+	case big.Int:
+		return t.Bytes(), nil
 	case *big.Int:
 		return t.Bytes(), nil
 	case int:
 		return toBytes(int64(t))
+	case uint:
+		return toBytes(uint64(t))
 	case string:
 		return []byte(t), nil
 	default:
 		var buf bytes.Buffer
+
 		err := binary.Write(&buf, binary.BigEndian, v)
 		if err != nil {
 			return nil, err
--- pkg/rlp/rlp_test.go
+++ pkg/rlp/rlp_test.go
@@ -1,15 +1,172 @@
 package rlp
 
 import (
-	"fmt"
+	"bytes"
+	"math/big"
 	"testing"
 
 	orig "github.com/ethereum/go-ethereum/rlp"
 )
 
-func TestEncode(t *testing.T) {
-	v := []uint16{200, 300, 0}
+type test struct {
+	val interface{}
+}
 
-	fmt.Println(orig.EncodeToBytes(v))
-	fmt.Println(Encode(v))
+type namedByteType byte
+
+var (
+	veryBigInt     = new(big.Int).Add(new(big.Int).Lsh(big.NewInt(0xFFFFFFFFFFFFFF), 16), big.NewInt(0xFFFF))
+	veryVeryBigInt = new(big.Int).Exp(veryBigInt, big.NewInt(8), nil)
+)
+
+var tests = []test{
+	// booleans
+	{val: true},
+	{val: false},
+
+	// integers
+	{val: uint32(0)},
+	{val: uint32(127)},
+	{val: uint32(128)},
+	{val: uint32(256)},
+	{val: uint32(1024)},
+	{val: uint32(0xFFFFFF)},
+	{val: uint32(0xFFFFFFFF)},
+	{val: uint64(0xFFFFFFFF)},
+	{val: uint64(0xFFFFFFFFFF)},
+	{val: uint64(0xFFFFFFFFFFFF)},
+	{val: uint64(0xFFFFFFFFFFFFFF)},
+	{val: uint64(0xFFFFFFFFFFFFFFFF)},
+
+	// big integers (should match uint for small values)
+	{val: big.NewInt(0)},
+	{val: big.NewInt(1)},
+	{val: big.NewInt(127)},
+	{val: big.NewInt(128)},
+	{val: big.NewInt(256)},
+	{val: big.NewInt(1024)},
+	{val: big.NewInt(0xFFFFFF)},
+	{val: big.NewInt(0xFFFFFFFF)},
+	{val: big.NewInt(0xFFFFFFFFFF)},
+	{val: big.NewInt(0xFFFFFFFFFFFF)},
+	{val: big.NewInt(0xFFFFFFFFFFFFFF)},
+
+	{val: veryBigInt},
+	{val: veryVeryBigInt},
+
+	// non-pointer big.Int
+	{val: *big.NewInt(0)},
+	{val: *big.NewInt(0xFFFFFF)},
+
+	// byte arrays
+	{val: [0]byte{}},
+	{val: [1]byte{0}},
+	{val: [1]byte{1}},
+	{val: [1]byte{0x7F}},
+	{val: [1]byte{0x80}},
+	{val: [1]byte{0xFF}},
+	{val: [3]byte{1, 2, 3}},
+	{val: [57]byte{1, 2, 3}},
+
+	// byte slices
+	{val: []byte{}},
+	{val: []byte{0}},
+	{val: []byte{0x7E}},
+	{val: []byte{0x7F}},
+	{val: []byte{0x80}},
+	{val: []byte{1, 2, 3}},
+
+	// strings
+	{val: ""},
+	{val: "\x7E"},
+	{val: "\x7F"},
+	{val: "\x80"},
+	{val: "dog"},
+	{val: "Lorem ipsum dolor sit amet, consectetur adipisicing eli"},
+	{val: "Lorem ipsum dolor sit amet, consectetur adipisicing elit"},
+	{val: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur mauris magna, suscipit sed vehicula non, iaculis faucibus tortor. Proin suscipit ultricies malesuada. Duis tortor elit, dictum quis tristique eu, ultrices at risus. Morbi a est imperdiet mi ullamcorper aliquet suscipit nec lorem. Aenean quis leo mollis, vulputate elit varius, consequat enim. Nulla ultrices turpis justo, et posuere urna consectetur nec. Proin non convallis metus. Donec tempor ipsum in mauris congue sollicitudin. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Suspendisse convallis sem vel massa faucibus, eget lacinia lacus tempor. Nulla quis ultricies purus. Proin auctor rhoncus nibh condimentum mollis. Aliquam consequat enim at metus luctus, a eleifend purus egestas. Curabitur at nibh metus. Nam bibendum, neque at auctor tristique, lorem libero aliquet arcu, non interdum tellus lectus sit amet eros. Cras rhoncus, metus ac ornare cursus, dolor justo ultrices metus, at ullamcorper volutpat"},
+
+	// named byte type arrays
+	{val: [0]namedByteType{}},
+	{val: [1]namedByteType{0}},
+	{val: [1]namedByteType{1}},
+	{val: [1]namedByteType{0x7F}},
+	{val: [1]namedByteType{0x80}},
+	{val: [1]namedByteType{0xFF}},
+	{val: [3]namedByteType{1, 2, 3}},
+	{val: [57]namedByteType{1, 2, 3}},
+
+	// named byte type slices
+	{val: []namedByteType{}},
+	{val: []namedByteType{0}},
+	{val: []namedByteType{0x7E}},
+	{val: []namedByteType{0x7F}},
+	{val: []namedByteType{0x80}},
+	{val: []namedByteType{1, 2, 3}},
+
+	// slices
+	{val: []uint{}},
+	{val: []uint{1, 2, 3}},
+	{val: []interface{}{[]interface{}{}, [][]interface{}{{}}, []interface{}{[]interface{}{}, [][]interface{}{{}}}}},
+	{val: []string{"aaa", "bbb", "ccc", "ddd", "eee", "fff", "ggg", "hhh", "iii", "jjj", "kkk", "lll", "mmm", "nnn", "ooo"}},
+	{val: []interface{}{uint(1), uint(0xFFFFFF), []interface{}{[]uint{4, 5, 5}}, "abc"}},
+	{
+		val: [][]string{
+			{"asdf", "qwer", "zxcv"},
+			{"asdf", "qwer", "zxcv"},
+			{"asdf", "qwer", "zxcv"},
+			{"asdf", "qwer", "zxcv"},
+			{"asdf", "qwer", "zxcv"},
+			{"asdf", "qwer", "zxcv"},
+			{"asdf", "qwer", "zxcv"},
+			{"asdf", "qwer", "zxcv"},
+			{"asdf", "qwer", "zxcv"},
+			{"asdf", "qwer", "zxcv"},
+			{"asdf", "qwer", "zxcv"},
+			{"asdf", "qwer", "zxcv"},
+			{"asdf", "qwer", "zxcv"},
+			{"asdf", "qwer", "zxcv"},
+			{"asdf", "qwer", "zxcv"},
+			{"asdf", "qwer", "zxcv"},
+			{"asdf", "qwer", "zxcv"},
+			{"asdf", "qwer", "zxcv"},
+			{"asdf", "qwer", "zxcv"},
+			{"asdf", "qwer", "zxcv"},
+			{"asdf", "qwer", "zxcv"},
+			{"asdf", "qwer", "zxcv"},
+			{"asdf", "qwer", "zxcv"},
+			{"asdf", "qwer", "zxcv"},
+			{"asdf", "qwer", "zxcv"},
+			{"asdf", "qwer", "zxcv"},
+			{"asdf", "qwer", "zxcv"},
+			{"asdf", "qwer", "zxcv"},
+			{"asdf", "qwer", "zxcv"},
+			{"asdf", "qwer", "zxcv"},
+			{"asdf", "qwer", "zxcv"},
+			{"asdf", "qwer", "zxcv"},
+		},
+	},
+
+	// nil
+	{val: (*uint)(nil)},
+	{val: (*string)(nil)},
+	{val: (*[]byte)(nil)},
+	{val: (*[10]byte)(nil)},
+	{val: (*big.Int)(nil)},
+	{val: (*[]string)(nil)},
+	{val: (*[10]string)(nil)},
+	{val: (*[]interface{})(nil)},
+	{val: (*[]struct{ uint })(nil)},
+	{val: (*[]interface{})(nil)},
+}
+
+func TestEncode(t *testing.T) {
+	for i, example := range tests {
+		original, _ := orig.EncodeToBytes(example.val)
+		tiny := Encode(example.val)
+
+		if !bytes.Equal(tiny, original) {
+			t.Errorf("\ntest %d: output mismatch:\ngot   %X\nwant  %X\n", i, tiny, original)
+		}
+	}
 }
