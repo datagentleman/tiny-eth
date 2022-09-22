6b04de205728427d8eee28a8ec45c92f0b8a4c78
Damian Jankowski <damian@datagentleman.io>
2022-09-22 20:27:48 +0200

    Working on leveldb

--- go.mod
+++ go.mod
@@ -3,3 +3,8 @@ module github.com/datagentleman/tiny-eth
 go 1.18
 
 require github.com/ethereum/go-ethereum v1.10.23
+
+require (
+	github.com/golang/snappy v0.0.4 // indirect
+	github.com/syndtr/goleveldb v1.0.1-0.20210819022825-2ae1ddf74ef7 // indirect
+)
---
+++ pkg/db/leveldb/leveldb.go
@@ -0,0 +1,47 @@
+package leveldb
+
+import (
+	"github.com/syndtr/goleveldb/leveldb"
+	"github.com/syndtr/goleveldb/leveldb/util"
+)
+
+type Level struct {
+	db *leveldb.DB
+}
+
+func Open(file string) (*Level, error) {
+	db, err := leveldb.OpenFile(file, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Level{db: db}, nil
+}
+
+func (lvl *Level) Get(key []byte) ([]byte, error) {
+	val, err := lvl.db.Get(key, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return val, nil
+}
+
+func (lvl *Level) First(n uint64, prefix []byte) [][]byte {
+	iter := lvl.db.NewIterator(util.BytesPrefix(prefix), nil)
+
+	num := uint64(0)
+	res := [][]byte{}
+
+	for iter.Next() {
+		res = append(res, iter.Key())
+
+		num++
+		if num == n {
+			break
+		}
+	}
+
+	iter.Release()
+	return res
+}
